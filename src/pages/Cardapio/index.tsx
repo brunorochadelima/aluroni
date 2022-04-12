9bf808a994c861fdbb0cccf0128e8084901948c8
brunorochadelima <brunorocha33@live.com>
2022-04-12 13:27:59 -0300

    importando a logo

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import styles from "./Cardapio.module.scss";
+import logo from "../../assets/logo.svg"
 
 function Cardapio() {
   return (
     <main>
       <nav className={styles.menu}>
-        <img src="../assets/logo.svg" alt="Logo do aluroni" />
+      <img src={logo} alt="Logo do aluroni" />
       </nav>
     </main>
   );
