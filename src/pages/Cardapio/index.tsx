be4c1e1a3121143e6080a4abbe5be7113a241917
brunorochadelima <brunorocha33@live.com>
2022-04-14 12:16:34 -0300

    criando o buscador

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import styles from "./Cardapio.module.scss";
 import logo from "../../assets/logo.svg";
+import Buscador from "./buscador";
 
 function Cardapio() {
+  const [busca, setBusca] = React.useState("");
   return (
     <main>
       <nav className={styles.menu}>
         <img src={logo} alt="Logo do aluroni" />
       </nav>
       <header className={styles.header}>
-        <div className={styles.header__text}>
-          A casa do código e da massa
-        </div>
+        <div className={styles.header__text}>A casa do código e da massa</div>
       </header>
+      <section className={styles.cardapio}>
+        <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+      <Buscador busca={busca} setBusca={setBusca} />
+      </section>
     </main>
   );
 }
