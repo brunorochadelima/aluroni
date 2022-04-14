0e2aaa9df627009b6374e97029063f15e0ccb4c2
brunorochadelima <brunorocha33@live.com>
2022-04-14 09:49:18 -0300

    criando o header

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import styles from "./Cardapio.module.scss";
-import logo from "../../assets/logo.svg"
-
-
+import logo from "../../assets/logo.svg";
 
 function Cardapio() {
   return (
     <main>
       <nav className={styles.menu}>
-      <img src={logo} alt="Logo do aluroni" />
+        <img src={logo} alt="Logo do aluroni" />
       </nav>
+      <header className={styles.header}>
+        <div className={styles.header__text}>
+          A casa do código e da massa
+        </div>
+      </header>
     </main>
   );
 }
