b54e25309529c2b1cabf520be05555ac373f5548
brunorochadelima <brunorocha33@live.com>
2022-04-24 10:49:57 -0300

    Adicionando itens dinamicamente

--- src/pages/Cardapio/Itens/item/index.tsx
+++ src/pages/Cardapio/Itens/item/index.tsx
@@ -1,23 +1,35 @@
-import React from 'react'
-import logo from '../../../../assets/logo.svg'
-import styles from '../item/item.module.scss'
+import cardapio from "../itens.json";
+import React from "react";
+import logo from "../../../../assets/logo.svg";
+import styles from "../item/item.module.scss";
 
-export default function Item() {
+type Props = typeof cardapio[0];
+
+export default function Item(props: Props) {
+  const { title, description, category, size, serving, price, photo } = props;
   return (
     <div className={styles.item}>
       <div className={styles.item__imagem}>
-        <img src={logo} alt="imagem" />
+        <img src={photo} alt="imagem" />
       </div>
       <div className={styles.item__descricao}>
         <div className={styles.item__titulo}>
-          <h2> Macarrão </h2>
-          <p> Descrição do macarrão </p>
+          <h2>{title}</h2>
+          <p>{description}</p>
         </div>
         <div className={styles.item__tags}>
-          <div className={styles.item__tipo}>Massa</div>
-          <div className={styles.item__porcao}>400g</div>
-          <div className={styles.item__qtdpessoas}>Serve 2 pessoas</div>
-          <div className={styles.item__valor}>R$ 50</div>
+          <div
+            className={`${styles.item__tipo} ${
+              styles["item__tipo__" + category.label.toLowerCase()]
+            }`}
+          >
+            {category.label}
+          </div>
+          <div className={styles.item__porcao}>{size}g</div>
+          <div className={styles.item__qtdpessoas}>
+            Serve {serving} pessoa{serving === 1 ? "" : "s"}
+          </div>
+          <div className={styles.item__valor}>R${price.toFixed(2)}</div>
         </div>
       </div>
     </div>
