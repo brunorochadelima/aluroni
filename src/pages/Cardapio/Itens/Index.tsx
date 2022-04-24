e7836c1b5887d6ebe3b18b22a93a9e2f30f95d1b
brunorochadelima <brunorocha33@live.com>
2022-04-24 10:49:16 -0300

    Adicionando itens

--- src/pages/Cardapio/Itens/Index.tsx
+++ src/pages/Cardapio/Itens/Index.tsx
@@ -7,7 +7,7 @@ export default function Itens() {
   return (
     <div className={styles.itens}>
       {cardapio.map((item) => (
-        <Item key={item.id} />
+        <Item key={item.id} {...item}/>
       ))}
     </div>
   );
