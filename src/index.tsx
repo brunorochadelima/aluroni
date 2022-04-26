ad6ad3a5fe8d6b4649baa429e5c3d796e3acb2fc
Bruno Rocha de Lima <brunorocha33@live.com>
2022-04-26 14:22:13 -0300

    Ajustando erros deploy

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import 'normalize.css'
+import Cardapio from '../src/pages/Cardapio';
+//import Cardapio from 'pages/Cardapio';
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+     <Cardapio/>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
---
+++ src/pages/Cardapio/Filtros/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from "../Filtros/Filtros.module.scss";
+import filtros from "../Filtros/filtros.json";
+
+type IOpcao = typeof filtros[0];
+
+interface Props {
+  filtro: number | null;
+  setFiltro: React.Dispatch<React.SetStateAction<number | null>>;
+}
+
+export default function Filtros({ filtro, setFiltro }: Props) {
+  function selecionaFiltros(opcao: IOpcao) {
+    if (filtro == opcao.id) {
+      return setFiltro(null);
+    }
+    return setFiltro(opcao.id);
+  }
+
+  return (
+    <div className={styles.filtros}>
+      {filtros.map((opcao) => (
+        <button
+          className={`${styles.filtros__filtro} ${
+            filtro === opcao.id ? styles["filtros__filtro--ativo"] : ""
+          }`}
+          key={opcao.id}
+          onClick={() => selecionaFiltros(opcao)}
+        >
+          {opcao.label}
+        </button>
+      ))}
+    </div>
+  );
+}
