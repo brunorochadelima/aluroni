fd3b186a664902a616cad71366bbf07cd227fca6
Bruno Rocha de Lima <brunorocha33@live.com>
2022-04-26 14:39:16 -0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import 'normalize.css'
+import "normalize.css";
 import Cardapio from '../src/pages/Cardapio';
 //import Cardapio from 'pages/Cardapio';
 
