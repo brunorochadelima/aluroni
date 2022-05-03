27819b9f6ab59e4bec6d8cee5ba886345a34d7ce
Bruno Rocha de Lima <brunorocha33@live.com>
2022-05-03 14:32:19 -0300

    mudando href para link to

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ReactComponent as Logo } from 'assets/logo.svg';
-//import logo from 'assets/logo.svg';
 import styles from './Menu.module.scss';
+import { Link } from 'react-router-dom';
 
 export default function Menu() {
   const rotas = [
@@ -25,7 +25,7 @@ export default function Menu() {
       <ul className={styles.menu__list}>
         {rotas.map((rota, index) => (
           <li key={index} className={styles.menu__link}>
-            <a href={rota.to}>{rota.label}</a>
+            <Link to={rota.to}>{rota.label}</Link>
           </li>
         ))}
       </ul>
