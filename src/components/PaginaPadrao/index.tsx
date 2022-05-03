a46e2807a1d82c3ede14146970cb86316fd2b147
Bruno Rocha de Lima <brunorocha33@live.com>
2022-05-03 15:58:24 -0300

    ajustando PaginaPadrao para só aparecer em páginas com conteúdo

---
+++ src/components/PaginaPadrao/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './PaginaPadrao.module.scss';
+import { Outlet } from 'react-router-dom';
+
+export default function PaginaPadrao() {
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.header__text}>A casa do código e da massa</div>
+      </header>
+      <div>
+        <Outlet />
+      </div>
+    </>
+  );
+}
--- src/routes.js
+++ src/routes.js
@@ -3,21 +3,23 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Inicio from 'pages/Inicio';
 import Cardapio from 'pages/Cardapio';
 import Menu from 'components/menu';
-import styles from './Routes.module.scss';
+import PaginaPadrao from 'components/PaginaPadrao';
 
 export default function AppRouter() {
   return (
     <main>
       <Router>
         <Menu />
-        <header className={styles.header}>
-          <div className={styles.header__text}>A casa do código e da massa</div>
-        </header>
         <Routes>
-          <Route path="/" element={<Inicio />} />
-          <Route path="/cardapio" element={<Cardapio />} />
+          <Route path="/" element={<PaginaPadrao />}>
+            <Route index element={<Inicio />} />
+            <Route path="cardapio" element={<Cardapio />} />
+          </Route>
         </Routes>
       </Router>
     </main>
   );
 }
+
+// Páginas Início e Cardápio são relativos a Página Padrão, a página Padrão(banner com foto) só aparece se a página filho existir
+// é necessário declarar o atributo <Outlet /> na página padrão.
