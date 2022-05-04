e9ad5f278b07c3becd3d63f669f1e67675105a18
Bruno Rocha de Lima <brunorocha33@live.com>
2022-05-04 14:23:20 -0300

    criando voltar na página 404

--- src/routes.js
+++ src/routes.js
@@ -5,25 +5,28 @@ import Cardapio from 'pages/Cardapio';
 import Menu from 'components/menu';
 import PaginaPadrao from 'components/PaginaPadrao';
 import Sobre from 'pages/Sobre';
+import NotFound from 'pages/NotFound';
 import Footer from 'components/Footer';
 
 export default function AppRouter() {
   return (
-    <main>
+    <main className="container">
       <Router>
         <Menu />
         <Routes>
           <Route path="/" element={<PaginaPadrao />}>
             <Route index element={<Inicio />} />
             <Route path="cardapio" element={<Cardapio />} />
-            <Route path="sobre" element={<Sobre/>} />
+            <Route path="sobre" element={<Sobre />} />
           </Route>
+          <Route path="*" element={<NotFound />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </Router>
     </main>
   );
 }
 
-// Páginas Início e Cardápio são relativos a Página Padrão, a página Padrão(banner com foto) só aparece se a página filho existir
-// é necessário declarar o atributo <Outlet /> na página padrão.
+/* Páginas Início e Cardápio são relativos a Página Padrão, a página Padrão(banner com foto) só aparece se a página filho existir
+é necessário declarar o atributo <Outlet /> na página padrão.
+*/
