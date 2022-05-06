15fd66e8ecc2cbd335cc88a9aef42f32623df3ab
Bruno Rocha de Lima <brunorocha33@live.com>
2022-05-06 09:09:43 -0300

    Refatorando tags

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,9 +1,10 @@
+import TagsPrato from 'components/TagsPrato';
 import React from 'react';
 import { Prato } from 'types/Prato';
 import styles from './Item.module.scss';
 
 export default function Item(props: Prato) {
-  const { title, description, category, size, serving, price, photo } = props;
+  const { title, description, photo } = props;
   return (
     <div className={styles.item}>
       <div className={styles.item__imagem}>
@@ -14,20 +15,7 @@ export default function Item(props: Prato) {
           <h2>{title}</h2>
           <p>{description}</p>
         </div>
-        <div className={styles.item__tags}>
-          <div
-            className={`${styles.item__tipo} ${
-              styles['item__tipo__' + category.label.toLowerCase()]
-            }`}
-          >
-            {category.label}
-          </div>
-          <div className={styles.item__porcao}>{size}g</div>
-          <div className={styles.item__qtdpessoas}>
-            Serve {serving} pessoa{serving === 1 ? '' : 's'}
-          </div>
-          <div className={styles.item__valor}>R${price.toFixed(2)}</div>
-        </div>
+        <TagsPrato {...props} />
       </div>
     </div>
   );
--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import cardapio from 'data/cardapio.json';
 import styles from './Prato.module.scss';
-import classNames from 'classnames';
+import TagsPrato from 'components/TagsPrato';
 
 export default function Prato() {
   const { id } = useParams();
@@ -24,22 +24,7 @@ export default function Prato() {
         </div>
         <div className={styles.conteudo}>
           <p className={styles.conteudo__descricao}>{prato.description}</p>
-          <div className={styles.tags}>
-            <div
-              className={classNames({
-                [styles.tags__tipo]: true,
-                [styles[`tags__tipo__${prato.category.label.toLowerCase()}`]]:
-                  true,
-              })}
-            >
-              {prato.category.label}
-            </div>
-            <div className={styles.tags__porcao}>{prato.size}g</div>
-            <div className={styles.tags__qtdpessoas}>
-              Serve {prato.serving} pessoa{prato.serving === 1 ? '' : 's'}
-            </div>
-            <div className={styles.tags__valor}>R${prato.price.toFixed(2)}</div>
-          </div>
+          <TagsPrato {...prato} />
         </div>
       </section>
     </>
