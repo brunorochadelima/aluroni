b9a9eac088410cc1ca36dd2e84823060038827f2
Bruno Rocha de Lima <brunorocha33@live.com>
2022-07-27 17:11:13 -0300

    memorizando componente

--- src/pages/Cardapio/Buscador/index.tsx
+++ src/pages/Cardapio/Buscador/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo, useMemo } from 'react';
 import styles from './Buscador.module.scss';
 import { CgSearch } from 'react-icons/cg';
 
@@ -7,7 +7,8 @@ interface Props {
   setBusca: React.Dispatch<React.SetStateAction<string>>;
 }
 
-export default function Buscador({ busca, setBusca }: Props) {
+function Buscador({ busca, setBusca }: Props) {
+  const iconBuscador = useMemo(() => <CgSearch size={20} color="#4C4D5E" />,[]);
   return (
     <div className={styles.buscador}>
       <input
@@ -15,7 +16,9 @@ export default function Buscador({ busca, setBusca }: Props) {
         onChange={(evento) => setBusca(evento.target.value)}
         placeholder="Buscar"
       />
-      <CgSearch size={20} color="#4C4D5E" />
+      {iconBuscador}
     </div>
   );
 }
+
+export default memo(Buscador);
