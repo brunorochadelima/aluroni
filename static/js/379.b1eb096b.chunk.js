22fc726ffeee6eaa07e6f15e8c781162fa4e1090
brunorochadelima <brunorochadelima@users.noreply.github.com>
2022-07-28 12:18:47 +0000

    deploy: 765299c0a0d8d495fd3aa0aacac64125ba17e07d

---
+++ static/js/379.b1eb096b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkaluroni=self.webpackChunkaluroni||[]).push([[379,878],{942:function(e,n,u){function r(e,n,u){return n in e?Object.defineProperty(e,n,{value:u,enumerable:!0,configurable:!0,writable:!0}):e[n]=u,e}u.d(n,{Z:function(){return r}})}}]);
+//# sourceMappingURL=379.b1eb096b.chunk.js.map
