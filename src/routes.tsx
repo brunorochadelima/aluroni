51cf1a0574e339ea103eb5e6ecc7dc1ff212faca
Bruno Rocha de Lima <brunorocha33@live.com>
2022-07-28 08:58:35 -0300

    code splitting das páginas

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,29 +1,32 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Inicio from 'pages/Inicio';
-import Cardapio from 'pages/Cardapio';
 import Menu from 'components/menu';
-import PaginaPadrao from 'components/PaginaPadrao';
-import Sobre from 'pages/Sobre';
-import NotFound from 'pages/NotFound';
 import Footer from 'components/Footer';
-import Prato from 'pages/Prato';
+
+const Cardapio = lazy(() => import('pages/Cardapio'));
+const PaginaPadrao = lazy(() => import('components/PaginaPadrao'));
+const NotFound = lazy(() => import('pages/NotFound'));
+const Prato = lazy(() => import('pages/Prato'));
+const Sobre = lazy(() => import('pages/Sobre'));
 
 export default function AppRouter() {
   return (
     <main className="container">
       <Router>
-        <Menu />
-        <Routes>
-          <Route path="/" element={<PaginaPadrao />}>
-            <Route index element={<Inicio />} />
-            <Route path="cardapio" element={<Cardapio />} />
-            <Route path="sobre" element={<Sobre />} />
-          </Route>
-          <Route path="prato/:id" element={<Prato />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-        <Footer />
+        <Suspense fallback={<p>Carregando...</p>}>
+          <Menu />
+          <Routes>
+            <Route path="/" element={<PaginaPadrao />}>
+              <Route index element={<Inicio />} />
+              <Route path="cardapio" element={<Cardapio />} />
+              <Route path="sobre" element={<Sobre />} />
+            </Route>
+            <Route path="prato/:id" element={<Prato />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+          <Footer />
+        </Suspense>
       </Router>
     </main>
   );
